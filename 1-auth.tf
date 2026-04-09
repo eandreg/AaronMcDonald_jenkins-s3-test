398624bbdbd03a78ae07c4f86b63576d3b882c21
eandreg <andre_new_york@yahoo.com>
2026-04-09 14:39:06 -0400

    04/09/2026 v13

--- 1-auth.tf
+++ 1-auth.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-     backend "s3" {
-    bucket  = "tf-backend-4357232b"                 # Name of the S3 bucket
-    key     = "jenkins-test-013125.tfstate" # The name of the state file in the bucket
-    region  = "us-east-1"                          # Use a variable for the region
-    encrypt = true                                 # Enable server-side encryption (optional but recommended)
-  }
+  #   backend "s3" {
+  #   bucket  = "tf-backend-4357232b"                 # Name of the S3 bucket
+  #   key     = "jenkins-test-013125.tfstate" # The name of the state file in the bucket
+  #   region  = "us-east-1"                          # Use a variable for the region
+  #   encrypt = true                                 # Enable server-side encryption (optional but recommended)
+  # }
 }
 
 provider "aws" {
